0489bdb84139bea204c635ac572749e976d4ea9e
Simon Martinelli <simon@72.services>
2022-05-06 11:35:38 +0200

    Added Calendar

---
+++ src/main/java/app/sudel/ui/views/create/CalendarEntryDialog.java
@@ -0,0 +1,12 @@
+package app.sudel.ui.views.create;
+
+import com.vaadin.flow.component.dialog.Dialog;
+import org.vaadin.stefan.fullcalendar.Entry;
+import org.vaadin.stefan.fullcalendar.FullCalendarScheduler;
+
+public class CalendarEntryDialog extends Dialog {
+
+    public CalendarEntryDialog(FullCalendarScheduler calendar, Entry entry) {
+
+    }
+}
--- src/main/java/app/sudel/ui/views/create/CreateView.java
+++ src/main/java/app/sudel/ui/views/create/CreateView.java
@@ -1,24 +1,158 @@
 package app.sudel.ui.views.create;
 
 import app.sudel.ui.views.MainLayout;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.HasDynamicTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import org.vaadin.stefan.fullcalendar.CalendarView;
+import org.vaadin.stefan.fullcalendar.Entry;
+import org.vaadin.stefan.fullcalendar.FullCalendarScheduler;
+import org.vaadin.stefan.fullcalendar.ResourceEntry;
+import org.vaadin.stefan.fullcalendar.Timezone;
+
+import java.text.DateFormatSymbols;
+import java.time.DayOfWeek;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 
 @AnonymousAllowed
 @RouteAlias(value = "", layout = MainLayout.class)
 @Route(layout = MainLayout.class)
-public class CreateView extends HorizontalLayout implements HasDynamicTitle {
+public class CreateView extends VerticalLayout implements HasDynamicTitle {
+
+    private final FullCalendarScheduler calendar;
 
     public CreateView() {
+        setId("create-view");
 
+        TextField name = new TextField(getTranslation("event.name"));
+        TextField location = new TextField(getTranslation("event.location"));
+        TextArea details = new TextArea(getTranslation("event.details"));
+
+        calendar = createCalendar();
+        FormLayout toolbar = createToolbar();
+
+        Div calenderWithToolbar = new Div(toolbar, calendar);
+        calenderWithToolbar.setSizeFull();
+
+        add(new FormLayout(name, location, details), calenderWithToolbar);
+    }
+
+    private FullCalendarScheduler createCalendar() {
+        FullCalendarScheduler calendar = new FullCalendarScheduler();
+        calendar.setSizeFull();
+        calendar.setNowIndicatorShown(true);
+        calendar.setTimezone(Timezone.getSystem());
+        calendar.setLocale(Locale.getDefault());
+        calendar.setSchedulerLicenseKey("GPL-My-Project-Is-Open-Source");
+
+        calendar.setFirstDay(DayOfWeek.MONDAY);
+
+        calendar.addEntryClickedListener(event -> {
+            if (event.getEntry().getRenderingMode() != Entry.RenderingMode.BACKGROUND
+                    && event.getEntry().getRenderingMode() != Entry.RenderingMode.INVERSE_BACKGROUND) {
+                new CalendarEntryDialog(calendar, event.getEntry()).open();
+            }
+        });
+
+        calendar.addTimeslotsSelectedSchedulerListener(event -> {
+            ResourceEntry entry = new ResourceEntry();
+
+            entry.setStart(event.getStart());
+            if (event.isAllDay()) {
+                entry.setEnd(event.getStart());
+            } else {
+                entry.setEnd(event.getEnd());
+            }
+            entry.setAllDay(event.isAllDay());
+
+            new CalendarEntryDialog(calendar, entry).open();
+        });
+
+        return calendar;
+    }
+
+    private FormLayout createToolbar() {
+        Button buttonPrevious = new Button(VaadinIcon.ANGLE_LEFT.create(), e -> calendar.previous());
+        Button buttonToday = new Button(VaadinIcon.HOME.create(), e -> calendar.today());
+        Button buttonNext = new Button(VaadinIcon.ANGLE_RIGHT.create(), e -> calendar.next());
+        buttonNext.setIconAfterText(true);
+
+        DatePicker gotoDate = new DatePicker();
+        gotoDate.addValueChangeListener(event1 -> calendar.gotoDate(event1.getValue()));
+        gotoDate.getElement().getStyle().set("visibility", "hidden");
+        gotoDate.getElement().getStyle().set("position", "fixed");
+        gotoDate.setWeekNumbersVisible(true);
+
+        DatePicker.DatePickerI18n datePickerI18n = new DatePicker.DatePickerI18n();
+        DateFormatSymbols symbols = new DateFormatSymbols(Locale.getDefault());
+        datePickerI18n.setMonthNames(Arrays.asList(symbols.getMonths()));
+        datePickerI18n.setFirstDayOfWeek(1);
+        datePickerI18n.setWeekdays(Arrays.stream(symbols.getWeekdays()).filter(s -> !s.isEmpty()).collect(Collectors.toList()));
+        datePickerI18n.setWeekdaysShort(Arrays.stream(symbols.getShortWeekdays()).filter(s -> !s.isEmpty()).collect(Collectors.toList()));
+        datePickerI18n.setDateFormats("dd.MM.yyyy", "dd.MM.yy", "ddMMyyyy", "ddMMyy");
+        gotoDate.setI18n(datePickerI18n);
+
+        Button buttonDatePicker = new Button("", VaadinIcon.CALENDAR.create());
+        buttonDatePicker.getElement().appendChild(gotoDate.getElement());
+        buttonDatePicker.addClickListener(event -> gotoDate.open());
+
+        Select<SudelCalendarView> select = new Select<>();
+        select.setWidthFull();
+        select.setItems(List.of(SudelCalendarView.values()));
+        select.setValue(SudelCalendarView.DAY_GRID_MONTH);
+        select.setItemLabelGenerator(sudelCalendarView -> getTranslation(sudelCalendarView.getName()));
+        select.addValueChangeListener(e -> calendar.changeView(Objects.requireNonNullElse(e.getValue(), SudelCalendarView.DAY_GRID_MONTH)));
+
+        FormLayout toolbar = new FormLayout(buttonToday, buttonPrevious, buttonNext, buttonDatePicker, select);
+
+        toolbar.getElement().getStyle().set("margin-top", "0px");
+        toolbar.setResponsiveSteps(new FormLayout.ResponsiveStep("0", 1), new FormLayout.ResponsiveStep("25em", 5));
+
+        return toolbar;
     }
 
     @Override
     public String getPageTitle() {
         return getTranslation("find.date");
+    }
+
+    public enum SudelCalendarView implements CalendarView {
+
+        TIME_GRID_WEEK("timeGridWeek", "Week"),
+        DAY_GRID_MONTH("dayGridMonth", "Month");
+
+        private final String clientSideValue;
+        private final String name;
+
+        SudelCalendarView(String clientSideValue, String name) {
+            this.clientSideValue = clientSideValue;
+            this.name = name;
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String getClientSideValue() {
+            return clientSideValue;
+        }
     }
 }
 
