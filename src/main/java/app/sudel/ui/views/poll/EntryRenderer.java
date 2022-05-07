5bcb5255d6f394b5fd18e54dbcf38287fd6b7537
Simon Martinelli <simon@72.services>
2022-05-07 14:04:21 +0200

    EntryRenderer refactoring

--- src/main/java/app/sudel/ui/views/poll/EntryRenderer.java
+++ src/main/java/app/sudel/ui/views/poll/EntryRenderer.java
@@ -1,12 +1,13 @@
 package app.sudel.ui.views.poll;
 
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
-import com.vaadin.flow.dom.Element;
-import com.vaadin.flow.dom.ElementFactory;
 import org.vaadin.stefan.fullcalendar.Entry;
 
 import java.time.format.DateTimeFormatter;
@@ -32,7 +33,7 @@ public class EntryRenderer extends ComponentRenderer<HorizontalLayout, Entry> {
         infoLayout.setSpacing(false);
         infoLayout.setPadding(false);
 
-        infoLayout.getElement().appendChild(createDateTimeInfo(entry));
+        infoLayout.add(createDateTimeInfo(entry));
 
         cardLayout.add(infoLayout);
 
@@ -43,18 +44,34 @@ public class EntryRenderer extends ComponentRenderer<HorizontalLayout, Entry> {
         return cardLayout;
     }
 
-    private Element createDateTimeInfo(Entry entry) {
-        Element dateTimeInfo;
+    private Div createDateTimeInfo(Entry entry) {
+        Div div = new Div();
         if (entry.isAllDay()) {
-            dateTimeInfo = Element.createText(DATE_FORMATTER.format(entry.getStartWithOffset()));
+            if (entry.getStart().equals(entry.getEnd())) {
+                div.add(new Span(DATE_FORMATTER.format(entry.getStartWithOffset())));
+            } else {
+                div.add(new Span(DATE_FORMATTER.format(entry.getStartWithOffset()) +
+                        " - " +
+                        DATE_FORMATTER.format(entry.getEndWithOffset())));
+            }
         } else {
-            dateTimeInfo = ElementFactory.createStrong(
-                    DATE_FORMATTER.format(entry.getStartWithOffset()) +
-                            " " +
-                            TIME_FORMATTER.format(entry.getStartWithOffset()) +
-                            " - " +
-                            TIME_FORMATTER.format(entry.getEndWithOffset()));
+            if (entry.getStart().toLocalDate().equals(entry.getEnd().toLocalDate())) {
+                div.add(new Span(DATE_FORMATTER.format(entry.getStartWithOffset()) +
+                        " " +
+                        TIME_FORMATTER.format(entry.getStartWithOffset()) +
+                        " - " +
+                        TIME_FORMATTER.format(entry.getEndWithOffset())));
+            } else {
+                div.add(new Span(DATE_FORMATTER.format(entry.getStartWithOffset()) +
+                        " " +
+                        TIME_FORMATTER.format(entry.getStartWithOffset()) +
+                        " -"));
+                div.add(new Paragraph());
+                div.add(new Span(DATE_FORMATTER.format(entry.getEndWithOffset()) +
+                        " " +
+                        TIME_FORMATTER.format(entry.getEndWithOffset())));
+            }
         }
-        return dateTimeInfo;
+        return div;
     }
 }
